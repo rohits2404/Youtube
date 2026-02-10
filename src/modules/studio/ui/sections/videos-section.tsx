4c33feba5f89be6eaba73b9534dc5d501bca7f22
Rohit Sharma <rohits8731@gmail.com>
2026-02-10 23:54:54 +0530

    11: Infinite Loading

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,11 +1,28 @@
 "use client";
 
+import { InfiniteScroll } from "@/components/InfiniteScroll";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
+import { useRouter } from "next/navigation";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export const VideosSection = () => {
+    return (
+        <Suspense fallback={<p>Loading...</p>}>
+            <ErrorBoundary fallback={<p>Error...</p>}>
+                <VideosSectionSuspense />
+            </ErrorBoundary>
+        </Suspense>
+    )
+}
 
-    const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery({
+const VideosSectionSuspense = () => {
+
+    const router = useRouter();
+
+    const [videos,query] = trpc.studio.getMany.useSuspenseInfiniteQuery({
         limit: DEFAULT_LIMIT
     }, {
         getNextPageParam: (lastPage) => lastPage.nextCursor
@@ -13,7 +30,44 @@ export const VideosSection = () => {
 
     return (
         <div>
-            {JSON.stringify(data)}
+            <div className="border-y">
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="pl-6 w-127.5">Video</TableHead>
+                            <TableHead>Visibility</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Date</TableHead>
+                            <TableHead className="text-right">Views</TableHead>
+                            <TableHead className="text-right">Comments</TableHead>
+                            <TableHead className="text-right pr-6">Likes</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {videos.pages.flatMap((page) => page.items).map((video) => (
+                            <TableRow
+                            key={video.id}
+                            className="cursor-pointer"
+                            onClick={() => router.push(`/studio/videos/${video.id}`)}
+                            >
+                                <TableCell>{video.title}</TableCell>
+                                <TableCell>Visibility</TableCell>
+                                <TableCell>Status</TableCell>
+                                <TableCell>Date</TableCell>
+                                <TableCell className="text-right">Views</TableCell>
+                                <TableCell className="text-right">Comments</TableCell>
+                                <TableCell className="text-right pr-6">Likes</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
+            <InfiniteScroll
+            isManual
+            hasNextPage={query.hasNextPage}
+            isFetchingNextPage={query.isFetchingNextPage}
+            fetchNextPage={query.fetchNextPage}
+            />
         </div>
     )
 }
